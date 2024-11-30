99e0d786497d08719ca7dbe9eca78508105c68d7
Jonas Matser <github@jonasmatser.nl>
2024-11-30 10:02:27 +0100

    Demo dynamic system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bevy::{
     asset::{io::Reader, AssetLoader, AssetPath, LoadContext},
+    ecs::system::{DynParamBuilder, DynSystemParam, ParamBuilder, SystemState},
     prelude::*,
 };
 
@@ -13,32 +14,112 @@ fn main() {
         ..default()
     }));
 
-    app.add_systems(PreStartup, pre_startup)
-        .add_systems(Update, update_lua_script_loader);
+    app
+        // .add_systems(Update, update_lua_script_loader)
+        .add_systems(PreUpdate, update_lua_systems);
 
-    app.init_asset::<LuaScript>();
+    app.init_asset::<LuaFile>();
     app.init_asset_loader::<LuaScriptLoader>();
+
+    app.init_resource::<LuaSystems>();
 
     app.run();
 }
 
-fn pre_startup(mut schedules: ResMut<Schedules>, asset_server: Res<AssetServer>) {
-    for (label, schedule) in schedules.iter() {
-        println!("Schedule: {:?}", label);
+#[derive(Debug, Resource)]
+struct LuaSystems {
+    systems: Vec<Handle<LuaFile>>,
+}
+
+impl FromWorld for LuaSystems {
+    fn from_world(world: &mut World) -> Self {
+        let asset_server = world.get_resource::<AssetServer>().unwrap();
+        let mut systems = Vec::new();
+        systems.push(asset_server.load("test.lua"));
+        LuaSystems { systems }
     }
-
-    let script: Handle<LuaScript> = asset_server.load("test.lua");
-    println!("Script: {:?}", script);
-
-    schedules.get_mut(Update).unwrap().add_systems(update);
 }
 
-fn update() {
-    println!("Update");
+fn update_lua_systems(world: &mut World) {
+    let systems = {
+        let mut systems: Vec<LuaFile> = Vec::new();
+
+        let mut system_state: SystemState<(
+            EventReader<AssetEvent<LuaFile>>,
+            ResMut<Assets<LuaFile>>,
+            NonSendMut<LuaVm>,
+            ResMut<Schedules>,
+        )> = SystemState::new(world);
+        let (mut asset_events, lua_files, mut lua_vm, mut schedules) = system_state.get_mut(world);
+
+        for event in asset_events.read() {
+            match event {
+                AssetEvent::Added { id } => {
+                    if let Some(file) = lua_files.get(*id) {
+                        println!("Added: {:?}", file);
+
+                        systems.push(file.clone());
+
+                        let executor = lua_vm
+                            .lua
+                            .try_enter(|ctx| {
+                                let closure = Closure::load(ctx, None, file.bytes.as_slice())?;
+                                Ok(ctx.stash(Executor::start(ctx, closure.into(), ())))
+                            })
+                            .unwrap();
+                        lua_vm.lua.execute::<()>(&executor).unwrap();
+                    }
+                }
+                AssetEvent::Modified { id } => {
+                    if let Some(file) = lua_files.get(*id) {
+                        println!("Modified: {:?}", file);
+                    }
+                }
+                AssetEvent::Removed { id } => {
+                    if let Some(file) = lua_files.get(*id) {
+                        println!("Removed: {:?}", file);
+                    }
+                }
+                AssetEvent::Unused { id } => {
+                    if let Some(file) = lua_files.get(*id) {
+                        println!("Unused: {:?}", file);
+                    }
+                }
+                AssetEvent::LoadedWithDependencies { id } => {
+                    if let Some(file) = lua_files.get(*id) {
+                        println!("LoadedWithDependencies: {:?}", file);
+                    }
+                }
+            }
+        }
+        systems
+    };
+
+    let systems = systems
+        .iter()
+        .map(|system| {
+            println!("System: {:?}", system);
+            (DynParamBuilder::new(ParamBuilder::query::<&Camera>()),)
+                // ()
+                .build_state(world)
+                .build_system(|camera| {
+                    // .build_system(|| {
+                    println!("Hello from dynamic system");
+                })
+        })
+        .collect::<Vec<_>>();
+
+    let mut schedules = world.get_resource_mut::<Schedules>().unwrap();
+    for system in systems.into_iter() {
+        println!("Adding system to schedule {:?}", system.name());
+        schedules.entry(Update).add_systems(system);
+    }
 }
 
-#[derive(Asset, TypePath, Debug)]
-struct LuaScript {}
+#[derive(Asset, Clone, TypePath, Debug)]
+struct LuaFile {
+    bytes: Vec<u8>,
+}
 
 struct LuaScriptLoader;
 
@@ -64,7 +145,7 @@ enum LuaScriptLoaderError {
 }
 
 impl AssetLoader for LuaScriptLoader {
-    type Asset = LuaScript;
+    type Asset = LuaFile;
     type Settings = ();
     type Error = LuaScriptLoaderError;
 
@@ -76,9 +157,7 @@ impl AssetLoader for LuaScriptLoader {
     ) -> Result<Self::Asset, Self::Error> {
         let mut bytes = Vec::new();
         reader.read_to_end(&mut bytes).await?;
-        // let lua_script = ron::de::from_bytes::<LuaScript>(&bytes)?;
-        // Ok(lua_script)
-        Ok(LuaScript {})
+        Ok(LuaFile { bytes })
     }
 
     fn extensions(&self) -> &[&str] {
